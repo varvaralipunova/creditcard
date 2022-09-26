928585de9a05caef3fbc4b131e3d3ef4c4088601
Varvara Lipunova <varvaralipunova@Varvaras-MacBook-Air.local>
2022-09-25 17:53:01 -0700

    add more validation

---
+++ src/app/date-form-control.ts
@@ -0,0 +1,8 @@
+import { FormControl } from "@angular/forms";
+
+export class DateFormControl extends FormControl {
+  override setValue(value:string, options:any) {
+    console.log(value);
+    super.setValue(value, options)
+  }
+}
